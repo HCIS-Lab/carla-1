82775ab0fc3704954dc9eea199a67df8a18c1b61
Hank Kung <hank910140@gmail.com>
2021-12-21 00:34:59 +0800

    update bash script

--- PythonAPI/examples/run_generator.sh
+++ PythonAPI/examples/run_generator.sh
@@ -3,15 +3,18 @@ num_scenario=0
 
 # features
 num_weather=6
-declare -a weather
 
 # weather[0] = 'ClearNoon'
-map=('Town03')
+map=('None' 'Town01' 'Town02' 'Town03' 'Town04' 'Town05')
 weather=('ClearNoon' 'CloudyNoon' 'WetNoon' 'WetCloudyNoon' 'MidRainyNoon' 'HardRainNoon' 'ClearSunset' 'CloudySunset' 'WetSunset' 'WetCloudySunset' 'HardRainSunset')
 traj=('True' 'False')
-random_actor=('True' 'False')
+random_actor=('True')
 random_object=('True' 'False')
-name=('t9-1_1_c_r_r_r' '3')
+name=('1_t9-1_1_c_r_r_r' '3')
+
+IFS='_' read -ra DES <<< "$name"
+echo ${map[${DES[0]}]}
+
 ../../CarlaUE4.sh &
 sleep 15
 for((i=0; i<${#weather[@]}; i++))
@@ -25,7 +28,7 @@ do
 		        		python ../util/config.py --reload
 		        		sleep 10
 		                echo ${i}
-		                time python data_generator.py -map ${map[0]} -scenario_id ${name[0]} -weather ${weather[i]} -noise_trajectory ${traj[j]} -random_actors ${random_actor[k]} -random_objects ${random_object[l]}
+		                time python data_generator.py -map ${map[${DES[0]}]} -scenario_id ${name[0]} -weather ${weather[i]} -noise_trajectory ${traj[j]} -random_actors ${random_actor[k]} -random_objects ${random_object[l]}
 		                sleep 3
 		            	done
         		done
