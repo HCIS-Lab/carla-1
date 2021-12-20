dcc313bbc926fd8b735335217deca82fedd5c586
Hank Kung <hank910140@gmail.com>
2021-12-21 00:34:22 +0800

    update scenario_id

--- PythonAPI/examples/monitor_control.py
+++ PythonAPI/examples/monitor_control.py
@@ -1271,7 +1271,7 @@ class CameraManager(object):
             
             # enter your scenario id
             # and there are some format requirement need to clarify
-            scenario_name_map = {'m1':'Town01', 'm2':'Town02', 'm3':'Town03', 'm5':'Town04'}
+            scenario_name_map = {'1':'Town01', '2':'Town02', '3':'Town03', '4':'Town04', '5': 'Town05'}
             #scenario_name_road_type_id = {}
             scenario_name_is_traffic_light = {'1': 'true', '0':  'false'}
             scenario_name_actor_type = {'c': 'car', 't': 'truck', 'b': 'bike', 'm': 'motor', 'p': 'pedestrian'}
