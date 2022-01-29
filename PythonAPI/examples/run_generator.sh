c8663cce0b71288c25ab471d082f394c89dd17ea
Hank Kung <hank910140@gmail.com>
2022-01-29 15:26:10 +0800

    script

--- PythonAPI/examples/run_generator.sh
+++ PythonAPI/examples/run_generator.sh
@@ -6,36 +6,45 @@ num_weather=6
 
 # weather[0] = 'ClearNoon'
 map=('None' 'Town01' 'Town02' 'Town03' 'Town04' 'Town05')
-weather=('ClearNoon' 'CloudyNoon' 'WetNoon' 'WetCloudyNoon' 'MidRainyNoon' 'HardRainNoon' 'ClearSunset' 'CloudySunset' 'WetSunset' 'WetCloudySunset' 'HardRainSunset')
-traj=('False' 'True')
+
+weather=('ClearNoon' 'CloudyNoon' 'WetNoon' 'WetCloudyNoon' 'MidRainyNoon' 'HardRainNoon' 'SoftRainNoon'
+	'ClearSunset' 'CloudySunset' 'WetSunset' 'WetCloudySunset' 'MidRainSunset' 'HardRainSunset' 'SoftRainSunset'
+	'ClearNight' 'CloudyNight' 'WetNight' 'WetCloudyNight' 'MidRainyNight' 'HardRainNight' 'SoftRainNight')
 random_actor=('low' 'mid' 'high')
-random_object=('False' 'True')
 name=('3_t5-5_1_p_r_r_r')
 
 IFS='_' read -ra DES <<< "$name"
 
-../../CarlaUE4.sh &
-sleep 15
-for((i=0; i<${#weather[@]}; i++))
-do
-        for((j=0; j<${#traj[@]}; j++))
-        do
-        		for((k=0; k<${#random_actor[@]}; k++))
-        		do
-	        			for((l=0; l<${#random_object[@]}; l++))
-	        			do	
-		        		python ../util/config.py --reload
-		        		sleep 8
-		                echo ${i}
-		                echo ${j}
-		                echo ${k}
-		                echo ${l}
-		                time python data_generator.py -map ${map[${DES[0]}]} -scenario_id ${name[0]} -weather ${weather[i]} -noise_trajectory ${traj[j]} -random_actors ${random_actor[k]} -random_objects ${random_object[l]}
-		                sleep 3
-		            	done
-        		done
-        done	
-done
+function random_range()
+{
+    if [ "$#" -lt "2" ]; then
+        echo "Usage: random_range <low> <high>"
+        return
+    fi
+    low=$1
+    range=$(($2 - $1))
+    echo $(($low+$RANDOM % $range))
+}
+a=$(random_range 0 6)
+b=$(random_range 7 13)
+c==$(random_range 14 20)
+w=(weather[$a] weather[$b] weather[$c])
+echo $w[*]
+# echo (${weather[$(random_range 0 20)]})
+# ../../CarlaUE4.sh &
+# sleep 15
+# for((i=0; i<${#weather[@]}; i++))
+# do
+# 		for((k=0; k<${#random_actor[@]}; k++))
+# 		do
+#     		python ../util/config.py --reload
+#     		sleep 8
+#             echo ${i}
+#             echo ${k}
+#             time python data_generator.py -map ${map[${DES[0]}]} -scenario_id ${name[0]} -weather ${weather[i]} -random_actors ${random_actor[k]}
+#             sleep 3
+# 		done
+# done
 
 
 
