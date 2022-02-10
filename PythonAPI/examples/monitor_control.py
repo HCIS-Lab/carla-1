1a6ec83c4795f0dde592b42c8d4a3b9d93afbce8
Hank Kung <hank910140@gmail.com>
2022-02-10 21:06:28 +0800

    update description

--- PythonAPI/examples/monitor_control.py
+++ PythonAPI/examples/monitor_control.py
@@ -1536,14 +1536,8 @@ class CameraManager(object):
                         continue
                     scenario_name += "_" + input_option
 
-                    print("Input is_interactive:")
-                    for key in scenario_name_interaction:
-                        print(key + ': ' +
-                              scenario_name_interaction[key] + ' ')
-                    input_option = str(input())
-                    if input_option not in scenario_name_interaction:
-                        print("INVALID INPUT! RESTART NAMING SCENARIO.")
-                        continue
+                    print("is_interactive: 1: True")
+                    input_option = '1'
                     scenario_name += "_" + input_option
 
                     print("Input name_violated_rule:")
@@ -1554,6 +1548,7 @@ class CameraManager(object):
                     if input_option not in scenario_name_violated_rule:
                         print("INVALID INPUT! RESTART NAMING SCENARIO.")
                         continue
+
                     scenario_name += "_" + input_option
                     scenario_num = 0
                     path = os.path.join(
@@ -1610,23 +1605,14 @@ class CameraManager(object):
                         continue
                     scenario_name += "_" + input_option
 
-                    print("Input actor_type:")
+                    print("actor_type: 0: None")
                     for key in scenario_name_actor_type:
                         print(key + ': ' + scenario_name_actor_type[key] + ' ')
-                    input_option = str(input())
-                    if input_option not in scenario_name_actor_type:
-                        print("INVALID INPUT! RESTART NAMING SCENARIO.")
-                        continue
+                    input_option = '0'
                     scenario_name += "_" + input_option
 
-                    print("Input actor_action:")
-                    for key in scenario_name_actor_type_action:
-                        print(key + ': ' +
-                              scenario_name_actor_type_action[key] + ' ')
-                    input_option = str(input())
-                    if input_option not in scenario_name_actor_type_action:
-                        print("INVALID INPUT! RESTART NAMING SCENARIO.")
-                        continue
+                    print("actor_action: 0: None")
+                    input_option = '0'
                     scenario_name += "_" + input_option
 
                     print("Input name_my_action:")
@@ -1638,14 +1624,8 @@ class CameraManager(object):
                         continue
                     scenario_name += "_" + input_option
 
-                    print("Input is_interactive:")
-                    for key in scenario_name_interaction:
-                        print(key + ': ' +
-                              scenario_name_interaction[key] + ' ')
-                    input_option = str(input())
-                    if input_option not in scenario_name_interaction:
-                        print("INVALID INPUT! RESTART NAMING SCENARIO.")
-                        continue
+                    print("is_interactive: 0: False")
+                    input_option = '0'
                     scenario_name += "_" + input_option
 
                     print("Input name_violated_rule:")
@@ -1657,6 +1637,7 @@ class CameraManager(object):
                         print("INVALID INPUT! RESTART NAMING SCENARIO.")
                         continue
                     scenario_name += "_" + input_option
+
                     scenario_num = 0
                     path = os.path.join(
                         'data_collection', self.scenario_type, scenario_name)
@@ -2012,36 +1993,37 @@ def save_actor(stored_path, actor_dict, control_dict, timestamp_list, obstacle_l
 
 def save_description(stored_path, scenario_type, scenario_name, carla_map):
     description = scenario_name.split('_')
+    topo = description[1].split('-')[0]
+    d = dict()
+
+    if 'i' in topo:
+        d['topology'] = '4_way_intersection'
+    elif 't' in topo:
+        if topo[1] == '1':
+            d['topology'] = '4_way_intersection_1'
+        elif topo[1] == '2':
+            d['topology'] = '4_way_intersection_2'
+        elif topo[1] == '3':
+            d['topology'] = '4_way_intersection_3'
+    elif 'r' in topo:
+        d['topology'] = 'roundabout'
+    elif 's' in topo:
+        d['topology'] = 'straight'
+
+
     if scenario_type == 'interactive':
         # [topology_id, is_traffic_light, actor_type_action, my_action, violated_rule]
-        actor = {'c': 'car', 't': 'truck', 'b': 'bike',
-                 'm': 'motor', 'p': 'pedestrian', '0': 'None'}
+        actor = {'c': 'car', 't': 'truck', 'b': 'bike', 'm': 'motor', 'p': 'pedestrian'}
 
         action = {'f': 'foward', 'l': 'left_turn', 'r': 'right_turn', 'sl': 'slide_left',
                   'sr': 'slide_right', 'u': 'u-turn', 's': 'stop', 'b': 'backward', 'c': 'crossing',
-                  'w': 'walking on sidewalk', 'j': 'jaywalking', '0': 'None'}
+                  'w': 'walking on sidewalk', 'j': 'jaywalking'}
 
         violation = {'0': 'None', 'p': 'parking', 'j': 'jay-walker', 'rl': 'running traffic light',
                      's': 'driving on a sidewalk', 'ss': 'stop sign'}
 
-        # 1: non-interactive, 2: interactive
-        interaction = {'0': 'False', '1': 'True'}
-        d = dict()
-        topo = description[1].split('-')[0]
-        if 'i' in topo:
-            d['topology'] = '4_way_intersection'
-        elif 't' in topo:
-            if topo[1] == '1':
-                d['topology'] = '4_way_intersection_1'
-            elif topo[1] == '2':
-                d['topology'] = '4_way_intersection_2'
-            elif topo[1] == '3':
-                d['topology'] = '4_way_intersection_3'
-        elif 'r' in topo:
-            d['topology'] = 'roundabout'
-        elif 's' in topo:
-            d['topology'] = 'straight'
-
+        interaction = {'1': 'True'}
+        
         d['traffic_light'] = 1 if description[2] == '1' else 0
         d['interaction_actor_type'] = actor[description[3]]
         d['interaction_action_type'] = action[description[4]]
@@ -2050,10 +2032,31 @@ def save_description(stored_path, scenario_type, scenario_name, carla_map):
         d['violation'] = violation[description[7]]
         d['map'] = carla_map
 
-        with open(stored_path + '/scenario_description.json', 'w') as f:
-            json.dump(d, f)
 
-    if scenario_type == 'obstacle':
+
+    elif scenario_type == 'non-interactive':
+        # [topology_id, is_traffic_light, actor_type_action, my_action, violated_rule]
+        actor = {'0': 'None'}
+
+        action = {'f': 'foward', 'l': 'left_turn', 'r': 'right_turn', 'sl': 'slide_left',
+                  'sr': 'slide_right', 'u': 'u-turn', 's': 'stop', 'b': 'backward', 'j': 'jaywalking', '0': 'None'}
+
+        violation = {'0': 'None', 'rl': 'running traffic light',
+                     's': 'driving on a sidewalk', 'ss': 'stop sign'}
+
+        interaction = {'0': 'False'}
+        
+        d['traffic_light'] = 1 if description[2] == '1' else 0
+        d['interaction_actor_type'] = actor[description[3]]
+        d['interaction_action_type'] = action[description[4]]
+        d['my_action'] = action[description[5]]
+        d['interaction'] = interaction[description[6]]
+        d['violation'] = violation[description[7]]
+        d['map'] = carla_map
+
+
+
+    elif scenario_type == 'obstacle':
         initial_action = {'f': 'foward', 'l': 'left_turn', 'r': 'right_turn', 'sl': 'slide_left', 'sr': 'slide_right', 
                             'u': 'u-turn', 's': 'stop', 'b': 'backward', 'c': 'crossing'}
 
@@ -2063,30 +2066,13 @@ def save_description(stored_path, scenario_type, scenario_name, carla_map):
         obstacle_type = {'0': 'traffic cone',
                          '1': 'street barrier', '2': 'traffic warning', '3': 'illegal parking'}
 
-        d = dict()
-        topo = description[1].split('-')[0]
-        if 'i' in topo:
-            d['topology'] = '4_way_intersection'
-        elif 't' in topo:
-            if topo[1] == '1':
-                d['topology'] = '4_way_intersection_1'
-            elif topo[1] == '2':
-                d['topology'] = '4_way_intersection_2'
-            elif topo[1] == '3':
-                d['topology'] = '4_way_intersection_3'
-        elif 'r' in topo:
-            d['topology'] = 'roundabout'
-        elif 's' in topo:
-            d['topology'] = 'straight'
-
         d['obstacle type'] = obstacle_type[description[2]]
         d['my_initial_action'] = initial_action[description[3]]
         d['my_action'] = action[description[4]]
         d['map'] = carla_map
 
-        with open(stored_path + '/scenario_description.json', 'w') as f:
-            json.dump(d, f)
-    if scenario_type == 'collision':
+
+    elif scenario_type == 'collision':
         # [topology_id, is_traffic_light, actor_type_action, my_action, violated_rule]
 
         actor = {'c': 'car', 't': 'truck', 'b': 'bike',
@@ -2099,24 +2085,6 @@ def save_description(stored_path, scenario_type, scenario_name, carla_map):
         violation = {'0': 'None', 'p': 'parking', 'j': 'jay-walker', 'rl': 'running traffic light',
                      's': 'driving on a sidewalk', 'ss': 'stop sign'}
 
-        # 1: non-interactive, 2: interactive
-        # interaction = {'0': 'False', '1': 'True'}
-        d = dict()
-        topo = description[1].split('-')[0]
-        if 'i' in topo:
-            d['topology'] = '4_way_intersection'
-        elif 't' in topo:
-            if topo[1] == '1':
-                d['topology'] = '4_way_intersection_1'
-            elif topo[1] == '2':
-                d['topology'] = '4_way_intersection_2'
-            elif topo[1] == '3':
-                d['topology'] = '4_way_intersection_3'
-        elif 'r' in topo:
-            d['topology'] = 'roundabout'
-        elif 's' in topo:
-            d['topology'] = 'straight'
-
         d['traffic_light'] = 1 if description[2] == '1' else 0
         d['interaction_actor_type'] = actor[description[3]]
         d['interaction_action_type'] = action[description[4]]
@@ -2124,8 +2092,10 @@ def save_description(stored_path, scenario_type, scenario_name, carla_map):
         # d['interaction'] = interaction[description[6]]
         d['violation'] = violation[description[6]]
         d['map'] = carla_map
-        with open(stored_path + '/scenario_description.json', 'w') as f:
-            json.dump(d, f)
+
+
+    with open(stored_path + '/scenario_description.json', 'w') as f:
+        json.dump(d, f)
     
 
 
