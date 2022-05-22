640008fe36b1b154551df4a36596cca1f8bba3b5
ChiehChi Yang <q23365292@gmail.com>
2022-05-22 21:53:08 +0800

    a script to check if the num of variance_scenario less than 4

--- PythonAPI/examples/test_scenario.sh
+++ PythonAPI/examples/test_scenario.sh
@@ -31,28 +31,13 @@ len=${#scenario_type}
 len=$((len + 19))
 folder=`ls -d ./data_collection/${scenario_type}/*`
 
-../../CarlaUE4.sh &
-sleep 15
-SERVICE="CarlaUE4"
+mkdir "./data_collection/${scenario_type}_fail/"
 for eachfile in $folder
 do
-    if pgrep "$SERVICE" >/dev/null
-    then
-        echo "$SERVICE is running"
-    else
-        echo "$SERVICE is  stopped"
-        ../../CarlaUE4.sh & sleep 15	
+    echo "$eachfile/variant_scenario"
+    check=`ls $eachfile/variant_scenario | wc -l`
+	if [ ${check} -lt 4 ]
+	then
+		mv $eachfile ./data_collection/${scenario_type}_fail/
     fi
-    #echo ${scenario_type}
-    echo ${eachfile:$len} 
-
-    if [ `echo ${eachfile:$len:2} | awk -v tem="10" '{print($1==tem)? "1":"0"}'` -eq "1" ]
-    then
-        python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map Town10HD --no_save
-    else
-        python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map Town0${eachfile:$len:1} --no_save
-    fi
-
-    sleep 3
-    #rm -r ./data_collection/${scenario_type}/${eachfile:$len}/ClearNoon_
 done
