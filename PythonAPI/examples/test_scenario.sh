528fb0a858f21838c182cfcd513ba72e86b138ca
Pang-Yuan Pao <55104237+WaywayPao@users.noreply.github.com>
2023-07-31 16:44:53 +0800

    Update test_scenario.sh

--- PythonAPI/examples/test_scenario.sh
+++ PythonAPI/examples/test_scenario.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 # This shell is going to create the video for each scenario_ID
 
+# kill al carla server 
+killall -9 -r CarlaUE4-Linux
+
 echo "Input the scenario_type you want to process"
 echo "Choose from the following options:"
-echo "1 - collision"
-echo "2 - obstacle"
-echo "3 - interactive"
-echo "4 - non-interactive"
-read -p "Enter scenario type ID to create a data video: " ds_id
 
+
+echo "1 - interactive"
+echo "2 - non-interactive"
+echo "3 - obstacle"
+echo "4 - collision"
+
+ds_id=1
+read -p "Enter scenario type ID to create a data video: " ds_id
 scenario_type="interactive"
 if [ ${ds_id} == 1 ]
 then
-    scenario_type="collision"
+    scenario_type="interactive"
 elif [ ${ds_id} == 2 ]
 then
-    scenario_type="obstacle"
+    scenario_type="non-interactive"
 elif [ ${ds_id} == 3 ]
 then
-    scenario_type="interactive"
+    scenario_type="obstacle"
 elif [ ${ds_id} == 4 ]
 then
-    scenario_type="non-interactive"
+    scenario_type="collision"
 else
     echo "Invalid ID!!!"
     echo "run default setting : interactive"
@@ -43,16 +49,16 @@ do
         echo "$SERVICE is  stopped"
         ../../CarlaUE4.sh & sleep 15	
     fi
-    #echo ${scenario_type}
+    
     echo ${eachfile:$len} 
 
-    if [ `echo ${eachfile:$len:2} | awk -v tem="10" '{print($1==tem)? "1":"0"}'` -eq "1" ]
-    then
-        python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map Town10HD --no_save
-    else
-        python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map Town0${eachfile:$len:1} --no_save
-    fi
+    python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map A1
+    # if [ `echo ${eachfile:$len:2} | awk -v tem="10" '{print($1==tem)? "1":"0"}'` -eq "1" ]
+    # then
+    #     python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map Town10HD
+    # else
+    #     python data_generator.py --scenario_type ${scenario_type} --scenario_id ${eachfile:$len} --map Town0${eachfile:$len:1}
+    # fi
 
     sleep 3
-    #rm -r ./data_collection/${scenario_type}/${eachfile:$len}/ClearNoon_
 done
